7f1441c8e4a8ab946db87875feb06b4d7ddb8583
50c10 <cit_java@hotmail.com>
2013-04-26 17:55:52 -0500

    modificacion en listado
    Se muestra la lista de herramientas que se pueden utilizar cuando un usuario esta logeado

--- tunasoft/tunasoft/apps/herramientas/models.py
+++ tunasoft/tunasoft/apps/herramientas/models.py
@@ -4,3 +4,7 @@ from django.db import models
 class herramienta(models.Model):
 	nombre	= models.CharField(max_length=50)
 	url		= models.URLField()
+	status	= models.BooleanField(default=True)
+
+	def __unicode__(self):
+		return self.nombre
--- tunasoft/tunasoft/apps/herramientas/views.py
+++ tunasoft/tunasoft/apps/herramientas/views.py
@@ -2,7 +2,10 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.contrib.auth.decorators import login_required
+from tunasoft.apps.herramientas.models import herramienta
 
 @login_required(login_url='/login/')
 def listado_view(request):
-	return render_to_response('herramientas/listado.html', context_instance=RequestContext(request))
+	herramientas = herramienta.objects.filter(status=True)
+	ctx = {'herramientas':herramientas}
+	return render_to_response('herramientas/listado.html', ctx, context_instance=RequestContext(request))
